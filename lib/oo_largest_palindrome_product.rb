5bec88a7e9d17f817093a05b1436772563609a01
Shujaat Azim <shujaat.azim@gmail.com>
2019-08-09 10:38:02 -0400

    Done.

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -3,21 +3,19 @@
 class LargestPalindromeProduct
 
     def answer
-
-        min = 100
-        max = 999
-        palindrome = 0
-      
+    min = 100
+    max = 999
+    palindrome = 0
+    
         for i in (min...max)
-          for j in (min...max)
+            for j in (min...max)
             product = i * j
-              if product.digits.join.to_i == product && product > palindrome
+                if product.digits.join.to_i == product && product > palindrome
                 palindrome = product
-              end
+                end
             end
-          end
-        return palindrome
-      
+        end
+      return palindrome
     end
 
 end
